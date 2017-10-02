a1ce7cf5a829fcaa6613b53d1dd7037e10b0bdbe
bhscs-build <bhs.cs.build@gmail.com>
2017-10-02 07:34:01 -0700

    Populating starter project: Part 2

---
+++ FracCalc/src/fracCalc/FracCalc.java
@@ -0,0 +1,33 @@
+package fracCalc;
+
+public class FracCalc
+{
+	public static void main(String[] args)
+	{
+		// TODO: Read the input from the user and call produceAnswer
+		// with that input, printing the result
+
+	}
+
+	// ** IMPORTANT ** DO NOT DELETE THIS METHOD.
+	// ** IMPORTANT ** DO NOT CHANGE THE SIGNATURE OF THIS METHOD.
+	// 
+	// This method will be used to test your code.  It takes a String parameter
+	// and RETURNS (not prints) the result
+	//
+	// Parameter is a fraction string that needs to be evaluated.  For your program, this will be
+	// the user input obtained by main.
+	//      e.g. input ==> "1/2 + 3/4"
+	//        
+	// The method should return the result of the fraction after it has been calculated
+	//      e.g. return ==> "1_1/4"
+	public static String produceAnswer(String input)
+	{
+		// TODO: Implement this method to produce and return the solution to the input
+
+		return "";
+	}
+
+	// TODO: Fill in the space below with any helper methods that you think you will need
+
+}
